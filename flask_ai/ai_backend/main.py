c6b11095425b69b831f53b4b7dfc39c2347219bf
fulanii <yassineissoufou21@gmail.com>
2023-05-21 15:06:28 -0500

    added html css and nba pic after pull

--- flask_ai/ai_backend/main.py
+++ flask_ai/ai_backend/main.py
@@ -6,7 +6,7 @@ import openai
 # Load your API key from an environment variable or secret management service
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-response = openai.Completion.create(model="text-davinci-003", prompt="Say this is a test", temperature=0, max_tokens=7)
+response = openai.Completion.create(model="text-davinci-003", prompt="what is one piece")
 
 
 print(response)
